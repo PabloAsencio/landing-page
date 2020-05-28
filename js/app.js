85af29584eb5fce085de207d489fd2b296064241
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2020-05-28 10:44:41 +0200

    feat: Create function to populate the navbar-list

--- js/app.js
+++ js/app.js
@@ -1,57 +1,71 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-
+ *
+ */
+const sections = document.getElementsByTagName('section');
+const navbarList = document.getElementById('navbar__list');
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
+ *
+ */
+const createAnchor = (section) => {
+    const id = section.getAttribute('id');
+    const content = section.getAttribute('data-nav');
+    const anchor = document.createElement('A');
+    anchor.setAttribute('href', `#${id}`);
+    anchor.textContent = content;
+    return anchor;
+};
 
-
+const populateNavbarList = () => {
+    const newList = document.createDocumentFragment();
+    for (const section of sections) {
+        const li = document.createElement('LI');
+        const anchor = createAnchor(section);
+        li.appendChild(anchor);
+        newList.appendChild(li);
+    }
+    navbarList.innerHTML = '';
+    navbarList.appendChild(newList);
+};
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
-
-
+populateNavbarList();
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
