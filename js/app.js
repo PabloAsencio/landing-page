6af2aa4a890d73dbba84d21c337b68b4d15d2cf1
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2020-06-07 11:59:03 +0200

    feat: Add full navbar functionality
    Add styling to the active section
    Show the active section in the navbar
    Add smooth scrolling effect to the navbar links

--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@
  */
 const sections = document.getElementsByTagName('section');
 const navbarList = document.getElementById('navbar__list');
+const navbarLinks = navbarList.getElementsByTagName('li');
 
 /**
  * End Global Variables
@@ -31,9 +32,51 @@ const createAnchor = (section) => {
     const anchor = document.createElement('A');
     anchor.setAttribute('href', `#${id}`);
     anchor.textContent = content;
+    anchor.setAttribute('id', `${section.getAttribute('id')}-link`);
     return anchor;
 };
 
+// The active state is defined as:
+//   * the element is at least partially visible,
+//   * its top is in the upper half of the viewport or higher,
+//   *  its bottom is lower than the upper 10% of the viewport
+const isActive = (element) => {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top < 0.5 * window.innerHeight &&
+        rect.bottom > 0.1 * window.innerHeight
+    );
+};
+
+const isCloserToTop = (oneElement, anotherElement) => {
+    return (
+        !anotherElement ||
+        oneElement.getBoundingClientRect().bottom <
+            anotherElement.getBoundingClientRect().bottom
+    );
+};
+
+const setActiveLink = (section) => {
+    for (const link of navbarLinks) {
+        if (
+            section &&
+            `${section.getAttribute('id')}-link` ===
+                link.firstElementChild.getAttribute('id')
+        ) {
+            link.firstElementChild.classList.add('link--active');
+        } else {
+            link.firstElementChild.classList.remove('link--active');
+        }
+    }
+};
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
 const populateNavbarList = () => {
     const newList = document.createDocumentFragment();
     for (const section of sections) {
@@ -46,17 +89,34 @@ const populateNavbarList = () => {
     navbarList.appendChild(newList);
 };
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
-populateNavbarList();
 // Add class 'active' to section when near top of viewport
+const setActiveSection = () => {
+    let activeSection = null;
+    for (const section of sections) {
+        section.classList.remove('your-active-class');
+        if (isActive(section) && isCloserToTop(section, activeSection)) {
+            activeSection = section;
+        }
+    }
+    if (activeSection) {
+        activeSection.classList.add('your-active-class');
+    }
+    setActiveLink(activeSection);
+};
 
 // Scroll to anchor ID using scrollTO event
+const scrollToSection = (event, link) => {
+    event.preventDefault();
+    const target = link.getAttribute('href').substring(1);
+    const section = document.getElementById(target);
+    const rect = section.getBoundingClientRect();
+    const scrollOptions = {
+        left: rect.x,
+        top: rect.y,
+        behavior: 'smooth',
+    };
+    window.scrollBy(scrollOptions);
+};
 
 /**
  * End Main Functions
@@ -65,7 +125,13 @@ populateNavbarList();
  */
 
 // Build menu
-
+populateNavbarList();
 // Scroll to section on link click
+for (const link of navbarLinks) {
+    link.addEventListener('click', (event) =>
+        scrollToSection(event, link.firstElementChild)
+    );
+}
 
 // Set sections as active
+window.addEventListener('scroll', setActiveSection);
